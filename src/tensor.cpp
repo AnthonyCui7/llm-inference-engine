7a06d54097a96c59482f9e11c263a0de8e51da1f
AnthonyCui7 <anthonycui001@gmail.com>
2026-04-15 20:26:01 -0500

    Refactor tensor struct fields

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,34 +1,46 @@
-#include <cassert>
-
+#include <iostream>
 #include "tensor.hpp"
 
 int Tensor::total_size() const {
     int size = 1;
-    for (const int& dim : dims) {
-        size *= dim;
+    for (int i = 0; i < ndim; i++) {
+        size *= shape[i];
     }
     return size;
 }
 
-float& Tensor::at(int i, int j) {
-    return data[i * dims[1] + j];
-}
-
-float& Tensor::at(const std::vector<int>& indices) {
-    assert(indices.size() == dims.size());
+float& Tensor::at(std::initializer_list<int> indices) {
+    assert(indices.size() == ndim);
+    int arr[8];
+    int i = 0;
+    for (const int& val : indices) {
+        arr[i++] = val;
+    }
     int index = 0;
     int multiple = 1;
     for (int i = 0; i < indices.size(); i++) {
-        assert(indices[i] < dims[i] && indices[i] >= 0);
+        assert(arr[i] < shape[i] && arr[i] >= 0);
         for (int j = i + 1; j < indices.size(); j++) {
-            multiple *= dims[j];
+            multiple *= shape[j];
         }
-        index += indices[i] * multiple;
+        index += arr[i] * multiple;
         multiple = 1;
     }
     return data[index];
 }
 
-int Tensor::dimensions() const {
-    return dims.size();
+void Tensor::print() const {
+    std::cout << "Shape: [";
+    for (int i = 0; i < ndim; i++) {
+        std::cout << shape[i];
+        if (i < ndim - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    for (int i = 0; i < total_size(); i++) {
+        std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
 }
--- src/tensor.hpp
+++ src/tensor.hpp
@@ -2,17 +2,29 @@
 Header file for a tensor class. Designed to be memory efficient for large scale operations later with LLMs.
 */
 #pragma once
-#include <vector>
+
 #include <cstring>
+#include <cassert>
 
 
 struct Tensor {
     float* data;
-    std::vector<int> dims;
+    int shape[8];
+    int ndim;
 
-    Tensor(const std::vector<int>& dimensions) {
-        dims = dimensions;
-        data = new float[total_size()];
+    Tensor(std::initializer_list<int> dimensions) {
+        ndim = dimensions.size();
+        assert(ndim <= 8);
+        int i = 0;
+        for (const int& dim : dimensions) {
+            shape[i++] = dim;
+        }
+        data = new float[total_size()]();
+    }
+
+    Tensor() {
+        data = nullptr;
+        ndim = 0;
     }
 
     ~Tensor() {
@@ -21,7 +33,8 @@ struct Tensor {
 
     // copy constructor
     Tensor(const Tensor& other) {
-        dims = other.dims;
+        ndim = other.ndim;
+        std::memcpy(shape, other.shape, sizeof(shape));
         int size = total_size();
         data = new float[size];
         std::memcpy(data, other.data, size * sizeof(float));
@@ -32,8 +45,9 @@ struct Tensor {
         if (this == &other) {
             return *this;
         }
+        ndim = other.ndim;
         delete[] data;
-        dims = other.dims;
+        std::memcpy(shape, other.shape, sizeof(shape));
         int size = total_size();
         data = new float[size];
         std::memcpy(data, other.data, size * sizeof(float));
@@ -42,9 +56,6 @@ struct Tensor {
 
     // methods
     int total_size() const;
-
-    float& at(int i, int j);
-    float& at(const std::vector<int>& indices);
-
-    int dimensions() const;
+    float& at(std::initializer_list<int> indices);
+    void print() const;
 };
