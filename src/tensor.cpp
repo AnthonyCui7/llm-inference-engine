7659e9699bb480cbcce414028430fde41aa31a40
AnthonyCui7 <anthonycui001@gmail.com>
2026-06-25 16:47:56 -0700

    Add copy constructor/assignment and move constructor/assignment test

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -66,7 +66,7 @@ const float* Tensor::data_ptr() const {
 }
 
 float& Tensor::at(std::initializer_list<int> indices) {
-    assert(indices.size() == ndim);
+    assert(indices.size() == static_cast<size_t>(ndim));
     int arr[8];
     int i = 0;
     for (const int& val : indices) {
@@ -74,9 +74,9 @@ float& Tensor::at(std::initializer_list<int> indices) {
     }
     int index = 0;
     int multiple = 1;
-    for (int i = 0; i < indices.size(); i++) {
+    for (size_t i = 0; i < indices.size(); i++) {
         assert(arr[i] < shape[i] && arr[i] >= 0);
-        for (int j = i + 1; j < indices.size(); j++) {
+        for (size_t j = i + 1; j < indices.size(); j++) {
             multiple *= shape[j];
         }
         index += arr[i] * multiple;
--- tests/test_tensor.cpp
+++ tests/test_tensor.cpp
@@ -1,5 +1,6 @@
 #include <utility>
 #include <cassert>
+#include <cstring>
 #include <iostream>
 
 #include "tensor.hpp"
@@ -39,6 +40,13 @@ int main() {
     assert(B.stride(0) == 3);
     assert(B.stride(1) == 1);
 
+    for (int i = 0; i < 6; i++) {
+        B.data[i] = i;
+        int row = i / 3;
+        int col = i % 3;
+        assert(B.data[i] == B.at({row, col}));
+    }
+
     for (int i = 2; i < 8; i++) {
         assert(B.shape[i] == 0);
         assert(B.strides[i] == 0);
@@ -52,6 +60,72 @@ int main() {
     assert(B.dim() == 2);
     assert(B.is_contiguous() == true);
     assert(B.data_ptr() != nullptr);
+
+    // test copy constructor/assignment deep copy
+    Tensor C = B;
+    C.data[0] = 10;
+    assert(B.data[0] != 10 && B.data_ptr() != C.data_ptr());
+
+    B = C;
+    assert(B.data[0] == 10 && B.data_ptr() != C.data_ptr());
+
+    // test move constructor
+    float* stolen_data = B.data;
+    int stolen_shape[8];
+    int stolen_strides[8];
+    int stolen_ndim = B.ndim;
+    char stolen_name[32];
+    std::memcpy(stolen_shape, B.shape, sizeof(B.shape));
+    std::memcpy(stolen_strides, B.strides, sizeof(B.strides));
+    std::memcpy(stolen_name, B.name, sizeof(B.name));
+
+
+    Tensor D = std::move(B);
+    assert(B.data == nullptr);
+    assert(B.ndim == 0);
+    assert(D.data == stolen_data);
+    assert(D.ndim == stolen_ndim);
+
+    for (int i = 0; i < 8; i++) {
+        assert(B.shape[i] == 0);
+        assert(B.strides[i] == 0);
+        assert(D.shape[i] == stolen_shape[i]);
+        assert(D.strides[i] == stolen_strides[i]);
+    }
+
+    for (int i = 0; i < 32; i++) {
+        assert(B.name[i] == 0);
+        assert(D.name[i] == stolen_name[i]);
+    }
+
+    // test move assignment
+    stolen_data = C.data;
+    stolen_ndim = C.ndim;
+    std::memcpy(stolen_shape, C.shape, sizeof(C.shape));
+    std::memcpy(stolen_strides, C.strides, sizeof(C.strides));
+    std::memcpy(stolen_name, C.name, sizeof(C.name));
+
+    A = std::move(C);
+
+    assert(C.data == nullptr);
+    assert(C.ndim == 0);
+    assert(A.data == stolen_data);
+    assert(A.ndim == stolen_ndim);
+
+    for (int i = 0; i < 8; i++) {
+        assert(C.shape[i] == 0);
+        assert(C.strides[i] == 0);
+        assert(A.shape[i] == stolen_shape[i]);
+        assert(A.strides[i] == stolen_strides[i]);
+    }
+
+    for (int i = 0; i < 32; i++) {
+        assert(C.name[i] == 0);
+        assert(A.name[i] == stolen_name[i]);
+    }
+
+    assert(A.data[0] == 10);
+    
 
     std::cout << "Test finished" << std::endl;
 
